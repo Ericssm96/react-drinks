46914d47f799f68da543f8414a5f509a3b8163ab
Eric Stéfano <ericssm96@gmail.com>
2025-11-18 22:26:23 -0300

    router loader added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,12 @@
+import { useLoaderData } from "react-router-dom";
+
+export const loader = async () => {
+  return "something";
+}
+
 export const Landing = () => {
+  const data = useLoaderData();
+  console.log(data);
   return (
     <div>Landing</div>
   )
