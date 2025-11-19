1c615bc06a0b864dd03a1577a0ef744e05f1f11b
Eric Stéfano <ericssm96@gmail.com>
2025-11-18 23:01:09 -0300

    initial request set up

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,20 @@
 import { useLoaderData } from "react-router-dom";
+import axios from "axios";
+
+const cocktailSearchUrl = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`;
 
 export const loader = async () => {
-  return "something";
+
+  let searchTerm = "margarita";
+  const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  console.log(response);
+
+  return {drinks: response.data.drinks, searchTerm};
 }
 
 export const Landing = () => {
-  const data = useLoaderData();
-  console.log(data);
+  const {drinks, searchTerm} = useLoaderData();
+  console.log(drinks);
   return (
     <div>Landing</div>
   )
